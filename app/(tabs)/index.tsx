5774c1d7dc22bf99f8a7cadf4195556641cc7406
Mattias Arwidson <mattias@actionawareness.se>
2024-09-18 12:38:29 +0200

    first commit

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+const lexa1 = () => {
+  return (
+  
+    <View style={[styles.defaultViewMain,{backgroundColor: "lightblue", flex:1,}]}>
+    
+      <View style={[styles.defaultView,{paddingTop:0}]}>
+      <View style={[styles.defaultView,{backgroundColor: 'red', height:90,justifyContent:"center",alignItems:"center"}]}>
+      <Text style={[styles.defaultText,{}]}>RÖD</Text>
+      </View>
+      </View>
+      
+      <View style={[styles.defaultView, styles.defaultViewRow,{}]}>
+      <View style={[styles.defaultView,{backgroundColor: 'green', height:90, flex:1}]}> 
+      <Text style={[styles.defaultText,{}]}>GRÖN</Text>
+      </View>
+      <View style={[styles.defaultView]}/>
+      <View style={[styles.defaultView,{backgroundColor: 'yellow', flex:1}]}>  
+      </View>
+      </View>
+      
+      <View style={[styles.defaultView, styles.defaultViewRow,{}]}>
+      <View style={[styles.defaultView,{backgroundColor: 'red', height:45, flex:3}]}/>
+      <View style={[styles.defaultView]}/>
+      <View style={[styles.defaultView,{backgroundColor: 'black', flex:1}]}/>
+      </View>
+      
+      <View style={{flex:1,justifyContent:"flex-end", }}>
+      <View style={[styles.defaultView,{backgroundColor:'orange',height:30,alignItems:"flex-end",justifyContent:"center"}]}>
+      <Text style={[styles.defaultText,{}]}>ORANGE</Text>
+      </View>
+      </View>
+    </View>
+  );
+};
+
+export default lexa1;
+
+const styles = StyleSheet.create({
+  defaultText:{
+  fontSize:20,
+  paddingHorizontal:4
+  },
+  defaultViewMain:{
+  padding:2,
+  flexDirection:"column"
+  },  
+  defaultViewRow:{
+  flexDirection:"Row",
+  },  
+  defaultView:{
+  padding:2,
+  },
+  
+});
